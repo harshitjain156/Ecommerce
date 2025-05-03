588fc78d4d978a686ce74443996eee513935d068
Harshit jain <harshit2020cs198@abesit.edu.in>
2025-05-03 18:22:34 +0530

    issue reolved

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 app.use(morgan('combined'))
 app.use(
   cors({
-    origin: "*",
+    origin: "https://ecommerce-dress.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
