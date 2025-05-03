129d05bc1b2fb3586b49d84300f2924d12b0adca
Harshit jain <harshit2020cs198@abesit.edu.in>
2025-05-03 18:10:10 +0530

    updated frontend

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -46,7 +46,7 @@ function ProductImageUpload({
     const data = new FormData();
     data.append("my_file", imageFile);
     const response = await axios.post(
-      "http://localhost:5000/api/admin/products/upload-image",
+      "https://ecommerce-onms.onrender.com/api/admin/products/upload-image",
       data
     );
 
